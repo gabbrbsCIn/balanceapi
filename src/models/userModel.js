d3cbb0c0ac223d9781fedceb34b9f6dc58bff4fa
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-08-14 16:40:03 -0300

    feat: criado model inicial do usuário

---
+++ src/models/userModel.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require("sequelize");
+const connection = require("../config/database");
+
+const User = connection.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+
+
+module.exports = User;
