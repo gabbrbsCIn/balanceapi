4cf690f90e0134179df9333ec0f2026bef6d808e
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-08-21 15:31:11 -0300

    fix: Corrigida lógica condicional  do findUserByEmail na função userRegister

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -4,12 +4,10 @@ const jwt = require("jsonwebtoken");
 const HandlerError = require("../errors/handlerError");
 require("dotenv").config();
 
-const verifyPassword = async (rightPassword, inputPassword) => {
-  const passwordIsValid = await bcrypt.compare(inputPassword, rightPassword);
-  if (!passwordIsValid) {
-    throw new HandlerError("Senha inválida", 401);
+const verifyDataFields = (data) => {
+  if (!data.username || !data.password || !data.email) {
+    throw new HandlerError("Todos os campos são obrigatórios", 400);
   }
-  return passwordIsValid;
 };
 
 const findUserByEmail = async (email) => {
@@ -18,9 +16,45 @@ const findUserByEmail = async (email) => {
       email,
     },
   });
-  console.log(user)
 
   return user;
+};
+
+const generateHashPassword = async (password) => {
+  hashedPassword = await bcrypt.hash(password, 10);
+  return hashedPassword;
+};
+
+const userRegister = async (data) => {
+  const user = await findUserByEmail(data.email);
+  if (user) {
+    throw new HandlerError("Esse usuário já existe!", 400);
+  }
+  const hashedPassword = await generateHashPassword(data.password);
+  const userDataRegister = {
+    username: data.username,
+    password: hashedPassword,
+    email: data.email,
+  };
+  await User.create(userDataRegister);
+  return userDataRegister;
+};
+
+const sendSucessResponse = (res, data, message) => {
+  return res.status(200).json({ message: message, data: data });
+};
+
+const sendMessageError = (res, error) => {
+  console.log(error.statusCode);
+  return res.status(error.statusCode).json({ message: error.message });
+};
+
+const verifyPassword = async (inputPassword, rightPassword) => {
+  const passwordIsValid = await bcrypt.compare(inputPassword, rightPassword);
+  if (!passwordIsValid) {
+    throw new HandlerError("Senha inválida", 401);
+  }
+  return passwordIsValid;
 };
 
 const userAuthenticate = async (email, inputPassword) => {
@@ -28,8 +62,8 @@ const userAuthenticate = async (email, inputPassword) => {
   if (!user) {
     throw new HandlerError("E-mail inválido", 401);
   }
-  await verifyPassword(user.password, inputPassword);
-  console.log
+  await verifyPassword(inputPassword, user.password);
+  console.log;
   return user;
 };
 
@@ -45,41 +79,6 @@ const generateJWTToken = async (user) => {
   );
 
   return token;
-};
-
-const sendSucessResponse = (res, data) => {
-  return res.status(200).json({ message: "Usuário logado", data: data });
-};
-
-const sendMessageError = (res, error) => {
-  console.log(error.statusCode);
-  return res.status(error.statusCode).json({ message: error.message });
-};
-
-const generateHashPassword = async (password) => {
-  hashedPassword = await bcrypt.hash(password, 10);
-  return hashedPassword;
-};
-
-const userRegister = async (data) => {
-  await findUserByEmail(data.email);
-  if (findUserByEmail) {
-    throw new HandlerError("Usuário já cadastrado", 400);
-  }
-  const hashedPassword = await generateHashPassword(data.password);
-  const userDataRegister = {
-    username: data.username,
-    password: hashedPassword,
-    email: data.email,
-  };
-  const user = await User.create(userDataRegister);
-  return user;
-};
-
-const verifyDataFields = (data) => {
-  if (!data.username || !data.password || !data.email) {
-    throw new HandlerError("Todos os campos são obrigatórios", 400);
-  }
 };
 
 module.exports = {
