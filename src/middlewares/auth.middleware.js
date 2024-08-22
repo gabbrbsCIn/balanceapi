761933adb75d02aa928d775556fc535b52809d00
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-08-22 10:14:46 -0300

    refactor: Atualizada verificação no middleware de autenticação

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,19 +1,23 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const { checkTokenInBlackList } = require("../services/authServices");
 
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const verifyToken = (req, res, next) => {
   let token = req.headers["authorization"];
   if (!token) {
-    return res.status(403).send({ message: "Nenhum token foi fornecido!" });
+    return res
+      .status(403)
+      .send({ message: "Nenhum token válido foi fornecido!" });
   }
   if (token.startsWith("Bearer ")) {
     token = token.slice(7, token.length);
   }
 
   jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
-    if (err) {
+    const isTokenInBlackList = checkTokenInBlackList(token);
+    if (err || isTokenInBlackList) {
       return res.status(401).send({ message: "Token não autorizado!" });
     }
     req.user = decoded;
