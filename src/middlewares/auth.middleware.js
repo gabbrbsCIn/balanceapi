859557576aac3d733d491205e1a7431815e6162a
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-08-23 10:41:28 -0300

    refactor: atualizada estrutura do middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,30 +1,21 @@
-const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { checkTokenInBlackList } = require("../services/authServices");
+const {
+  extractTokenFromHeader,
+  sendMessageError,
+  verifyJWTToken,
+  checkTokenInBlackList,
+} = require("../services/authServices");
 
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-
-const verifyToken = (req, res, next) => {
-  let token = req.headers["authorization"];
-  if (!token) {
-    return res
-      .status(403)
-      .send({ message: "Nenhum token válido foi fornecido!" });
+const authenticateToken = (req, res, next) => {
+  try {
+    const token = extractTokenFromHeader(req.headers);
+    checkTokenInBlackList(token);
+    verifyJWTToken(req, token, next);
+  } catch (error) {
+    sendMessageError(res, error);
   }
-  if (token.startsWith("Bearer ")) {
-    token = token.slice(7, token.length);
-  }
-
-  jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
-    const isTokenInBlackList = checkTokenInBlackList(token);
-    if (err || isTokenInBlackList) {
-      return res.status(401).send({ message: "Token não autorizado!" });
-    }
-    req.user = decoded;
-    next();
-  });
 };
 
 module.exports = {
-  verifyToken,
+  authenticateToken,
 };
