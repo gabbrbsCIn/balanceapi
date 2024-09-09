3d3db029997b199e00630c1209dc3966b9d90453
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-09 09:48:51 -0300

    feat: add middleware de admin à rota para criação das sections

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-const adminControllers = require("../controllers/adminControllers")
+const adminControllers = require("../controllers/adminControllers");
 const { authenticateToken } = require("../middlewares/auth.middleware");
+const { admin } = require("../middlewares/admin.middleware");
 
 router.post("/condominium", authenticateToken, adminControllers.condominium);
-
-
+router.post("/section", authenticateToken, admin, adminControllers.section);
 
 module.exports = router;
