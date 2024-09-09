24879bb9791678c7dc89d983263f6cdf6bb271ee
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-09 11:18:11 -0300

    refactor: atualizado nome da função sendSuccessResponde

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,6 +1,13 @@
 const HandlerError = require("../errors/handlerError");
-const { checkDataFields, createCondominium} = require("../services/adminServices");
-const { sendSucessResponse, sendMessageError } = require("../services/authServices");
+const {
+  checkDataFields,
+  createCondominium,
+  createSection,
+} = require("../services/adminServices");
+const {
+  sendSuccessResponse,
+  sendMessageError,
+} = require("../services/authServices");
 
 const condominium = async (req, res) => {
   try {
@@ -8,21 +15,32 @@ const condominium = async (req, res) => {
     checkDataFields(name);
     const residentAdminId = req.user.id;
     const condominium = await createCondominium(name, residentAdminId);
-    sendSucessResponse(res, condominium, "Condomínio criado com sucesso");
+    sendSuccessResponse(res, condominium, "Condomínio criado com sucesso");
   } catch (error) {
-    if (error instanceof HandlerError){
-        sendMessageError(res, error);
+    if (error instanceof HandlerError) {
+      sendMessageError(res, error);
     } else {
-        res.send(error).status(500);
+      res.send(error).status(500);
     }
   }
 };
 
 const section = async (req, res) => {
-  return res.send(req.body).status(200);
+  try {
+    const { condominiumId, sectionName } = req.body;
+    console.log(checkDataFields(sectionName));
+    const section = await createSection(sectionName, condominiumId);
+    sendSuccessResponse(res, section, "Bloco criado com sucesso");
+  } catch (error) {
+    if (error instanceof HandlerError) {
+      sendMessageError(res, error);
+    } else {
+      res.send(error).status(500);
+    }
+  }
 };
 
 module.exports = {
   condominium,
-  section
+  section,
 };
