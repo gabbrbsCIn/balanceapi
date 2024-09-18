a20b00a8a0eb5abd50760b2f044c95cc93f72bf3
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-18 11:53:31 -0300

    feat: add rota para chamar controller residentInAparment

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -9,5 +9,6 @@ const { admin } = require("../middlewares/admin.middleware");
 router.post("/condominium", authenticateToken, adminControllers.condominium);
 router.post("/section", authenticateToken, admin, adminControllers.section);
 router.post("/apartment", authenticateToken, admin, adminControllers.apartment);
+router.patch("/apartment/resident", authenticateToken, admin, adminControllers.residentInAparment);
 
 module.exports = router;
