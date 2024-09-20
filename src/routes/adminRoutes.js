7909f944baa74e1f351dedf8368dab51958bb0fb
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-20 11:07:57 -0300

    feat: add rota para chamar controller transaction

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -10,5 +10,6 @@ router.post("/condominium", authenticateToken, adminControllers.condominium);
 router.post("/section", authenticateToken, admin, adminControllers.section);
 router.post("/apartment", authenticateToken, admin, adminControllers.apartment);
 router.patch("/apartment/resident", authenticateToken, admin, adminControllers.residentInAparment);
+router.post("/transaction", authenticateToken, admin, adminControllers.transaction);
 
 module.exports = router;
