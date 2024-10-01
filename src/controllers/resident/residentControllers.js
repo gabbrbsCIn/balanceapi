e728a3bc4a5bfa7d997082a911a00eb449ec14a4
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-10-01 11:21:50 -0300

    refactor: add parâmetro condominiumId às funções getTransactionsByFilter e getTransactionsFromCurrentMonth

--- src/controllers/resident/residentControllers.js
+++ src/controllers/resident/residentControllers.js
@@ -11,21 +11,27 @@ const {
   hasFilters,
   getTransactionsFromCurrentMonth,
   getTransactionsByFilter,
-  updateResident,
   updateResidentById,
 } = require("../../services/residentServices");
 
 const balance = async (req, res) => {
   try {
+    const condominiumId = req.condominiumId;
     const filterData = hasFilters(req);
     if (!filterData) {
       const today = new Date();
       const currentMonth = today.toISOString().slice(0, 7);
-      const transaction = await getTransactionsFromCurrentMonth(currentMonth);
+      const transaction = await getTransactionsFromCurrentMonth(
+        currentMonth,
+        condominiumId
+      );
       sendSucessResponse(res, transaction, "Transações Coletadas");
       return transaction;
     }
-    const transaction = await getTransactionsByFilter(filterData);
+    const transaction = await getTransactionsByFilter(
+      filterData,
+      condominiumId
+    );
     sendSucessResponse(res, transaction, "Transações Coletadas");
   } catch (error) {
     sendMessageError(res, error);
