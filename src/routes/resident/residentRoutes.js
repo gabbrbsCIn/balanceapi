91d33a2a709a8a1836b3d7a59b1413d0ab4494e2
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-10-01 09:05:08 -0300

    feat: add rota para chamar controller update

--- src/routes/resident/residentRoutes.js
+++ src/routes/resident/residentRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const residentControllers = require("../../controllers/resident/residentControllers");
 
 router.get("/:condominiumId/balance", residentControllers.balance);
+router.put("/:condominiumId/update", residentControllers.update);
 
 module.exports = router;
