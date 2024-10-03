7eaa9f7de500fd8b4c74a9a5a9625d93336badba
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-10-03 18:01:30 -0300

    refactor: atualizado paymentService para gerar qr-code pix de acordo com as credenciais do id da transação

--- src/services/paymentServices.js
+++ src/services/paymentServices.js
@@ -16,20 +16,31 @@ const options = {
       tax_id: "12345678909",
     },
     reference_id: "1",
-    items: [{ name: "Taxa de Condomínio", quantity: 1, unit_amount: 200 }],
+    items: [{ name: "Taxa de Condomínio", unit_amount: 200, quantity: 1 }],
     qr_codes: [
       { amount: { value: 4000 }, expiration_date: "2024-10-02T20:15:59-03:00" },
     ],
   },
 };
 
-const generatePixQrCode = async () => {
+const generatePixQrCode = async (transactionData, residentData) => {
   try {
+    options.data.customer.name = residentData.username;
+    options.data.customer.email = residentData.email;
+    options.data.reference_id = transactionData.id;
+
+    options.data.items[0].name = transactionData.name;
+    options.data.qr_codes[0].amount.value = transactionData.value;
+    let expirationDate = new Date();
+
+    expirationDate.setHours(expirationDate.getHours() + 1);
+    options.data.qr_codes[0].expiration_date = expirationDate;
     const response = await axios.request(options);
-    console.log(response.data);
-    return response.data;
+    console.log(response);
+    return response;
   } catch (error) {
-    console.log(error.response.data);
+    console.log(error);
+    console.log(error.response.data.error_messages);
     throw new HandlerError(
       error.response.data.error_messages,
       error.response.status
