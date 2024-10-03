6881e5d2b3f8e14894399303ecbf0a2814979796
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-10-03 15:00:31 -0300

    feat: add rota para chamar controller residentDebits

--- src/routes/resident/residentRoutes.js
+++ src/routes/resident/residentRoutes.js
@@ -3,9 +3,20 @@ const express = require("express");
 const router = express.Router();
 
 const residentControllers = require("../../controllers/resident/residentControllers");
-const { checkResidentInCondominium } = require("../../middlewares/resident.middleware");
+const {
+  checkResidentInCondominium,
+} = require("../../middlewares/resident.middleware");
 
-router.get("/:condominiumId/balance", checkResidentInCondominium, residentControllers.balance);
+router.get(
+  "/:condominiumId/balance",
+  checkResidentInCondominium,
+  residentControllers.balance
+);
+router.get(
+  "/:condominiumId/debits",
+  checkResidentInCondominium,
+  residentControllers.residentDebits
+);
 router.put("/:condominiumId/update", residentControllers.update);
 router.post("/:condominiumId/payment", residentControllers.payDebits);
 
