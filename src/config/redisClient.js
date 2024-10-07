3ba2c2ae49e63887dfa4c4e3473ccc894a9ab740
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-10-07 16:13:10 -0300

    feat: add configuração createClient Redis

---
+++ src/config/redisClient.js
@@ -0,0 +1,12 @@
+const { createClient } = require("redis");
+require("dotenv").config();
+
+const client = createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+});
+
+module.exports = client;
